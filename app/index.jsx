688a8441a812cceedbe128fd56db6986d04a1811
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-28 11:16:07 +0300

    .

---
+++ app/index.jsx
@@ -0,0 +1,32 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text> hello welcome to my app.</Text>
+      <Text>The future of android</Text>
+      <Link href="/about">about </Link>
+    </View>
+  );
+}
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+  },
+});
